d1301715fa4071afa3c0df2ee03f11ddd98a34e8
Marcus Mu <chunkhang@gmail.com>
2020-12-11 00:52:28 +0800

    Remove sourcing completion

--- rbenv-lazy.plugin.zsh
+++ rbenv-lazy.plugin.zsh
@@ -2,7 +2,6 @@
 if type rbenv &> /dev/null; then
   local RBENV_SHIMS="${RBENV_ROOT:-${HOME}/.rbenv}/shims"
   export PATH="${RBENV_SHIMS}:${PATH}"
-  source $(dirname $(greadlink -f `whence -p rbenv`))/../completions/rbenv.zsh
   function rbenv() {
     unset -f rbenv > /dev/null 2>&1
     eval "$(command rbenv init -)"
